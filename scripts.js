f50f0a2eac720334bcc8f4b45e404701567dc7b9
j00by <jhuynh@rebuildbydesign.org>
2024-07-01 07:38:11 -0400

    update .jpg extension senator

--- scripts.js
+++ scripts.js
@@ -71,7 +71,7 @@ map.on('load', function () {
                 15, '#a50f15',
                 '#ffffff' // Default color used if none of the values match
             ],
-            'fill-opacity': 0.8
+            'fill-opacity': 1
         }
     }, 'state-label');
 
