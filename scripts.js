4ae8b6acbecc1c92fecd320de042621d6b332054
Rebuild by Design <jhuynh@rebuildbydesign.org>
2024-07-09 14:43:15 -0400

    updated popup with county level per capita

--- scripts.js
+++ scripts.js
@@ -40,7 +40,7 @@ map.on('load', function () {
     // Load the GeoJSON file for Atlas_FEMA
     map.addSource('atlas-fema', {
         type: 'geojson',
-        data: 'data/Atlas_FEMA.json'
+        data: 'data/Atlas_FEMA2.json'
     });
 
     // Add a layer for the Atlas_FEMA data
@@ -113,50 +113,51 @@ map.on('load', function () {
         maxWidth: "none"  // Allows custom width styling
     });
 
-    // When a user clicks on the map, show a popup with information
-    map.on('click', function (e) {
-        var femaFeatures = map.queryRenderedFeatures(e.point, { layers: ['atlas-fema-layer'] });
-        var congressFeatures = map.queryRenderedFeatures(e.point, { layers: ['congress-layer'] });
+   // When a user clicks on the map, show a popup with information
+map.on('click', function (e) {
+    var femaFeatures = map.queryRenderedFeatures(e.point, { layers: ['atlas-fema-layer'] });
+    var congressFeatures = map.queryRenderedFeatures(e.point, { layers: ['congress-layer'] });
 
-        if (!femaFeatures.length || !congressFeatures.length) {
-            return;
-        }
+    if (!femaFeatures.length || !congressFeatures.length) {
+        return;
+    }
 
-        var femaFeature = femaFeatures[0].properties;
-        var congressFeature = congressFeatures[0].properties;
+    var femaFeature = femaFeatures[0].properties;
+    var congressFeature = congressFeatures[0].properties;
 
-        var stateName = femaFeature.STATE_NAME;
-        var countyName = femaFeature.COUNTY_NAME;
-        var disasterCount = femaFeature.COUNTY_DISASTER_COUNT;
-        var representativeName = `${congressFeature.FIRSTNAME} ${congressFeature.LASTNAME}`;
-        var party = congressFeature.PARTY;
-        var repImage = congressFeature.PHOTOURL;
-        var websiteUrl = congressFeature.WEBSITEURL;
-        var facebookUrl = congressFeature.FACE_BOOK_URL;
-        var twitterUrl = congressFeature.TWITTER_URL;
-        var instagramUrl = congressFeature.INSTAGRAM_URL;
-        var senator1 = congressFeature.SENATOR1;
-        var sen1party = congressFeature.SENATOR1_PARTY;
-        var senator1Url = congressFeature.SENATOR1_URL;
-        var senator2 = congressFeature.SENATOR2;
-        var sen2party = congressFeature.SENATOR2_PARTY;
-        var senator2Url = congressFeature.SENATOR2_URL;
-        var atlasUrl = femaFeature.ATLAS_URL;
-        var atlasCover = congressFeature.ATLAS_COVER;
+    var stateName = femaFeature.STATE_NAME;
+    var countyName = femaFeature.COUNTY_NAME;
+    var disasterCount = femaFeature.COUNTY_DISASTER_COUNT;
+    var representativeName = `${congressFeature.FIRSTNAME} ${congressFeature.LASTNAME}`;
+    var party = congressFeature.PARTY;
+    var repImage = congressFeature.PHOTOURL;
+    var websiteUrl = congressFeature.WEBSITEURL;
+    var facebookUrl = congressFeature.FACE_BOOK_;
+    var twitterUrl = congressFeature.TWITTER_UR;
+    var instagramUrl = congressFeature.INSTAGRAM_;
+    var senator1 = congressFeature.SENATOR1;
+    var sen1party = congressFeature.SENATOR1_PARTY;
+    var senator1Url = congressFeature.SENATOR1_URL;
+    var senator2 = congressFeature.SENATOR2;
+    var sen2party = congressFeature.SENATOR2_PARTY;
+    var senator2Url = congressFeature.SENATOR2_URL;
+    var atlasUrl = femaFeature.ATLAS_URL;
+    var atlasCover = congressFeature.ATLAS_COVER;
 
-        var femaCountyTotalFunds = femaFeature.FEMA_COUNTY_TOTAL_FUNDS;
-        var stateFemaTotalFunds = femaFeature.STATE_FEMA_TOTAL_FUNDS;
-        var stateCdbgTotalFunds = femaFeature.STATE_CDBG_TOTAL_FUNDS;
-        var statePopulation = femaFeature.STATE_POPULATION;
-        var statePerCapita = femaFeature.STATE_PER_CAPITA;
+    var countyFemaTotal = femaFeature.COUNTY_TOTAL_FEMA;
+    var countyPerCapita = femaFeature.COUNTY_PER_CAPITA;
+    var stateFemaTotal = femaFeature.STATE_FEMA_TOTAL;
+    var stateCdbgTotal = femaFeature.STATE_CDBG_TOTAL;
+    var statePerCapita = femaFeature.STATE_PER_CAPITA;
 
-        var formattedFemaCountyTotalFunds = `$${Number(femaCountyTotalFunds).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
-        var formattedStateFemaTotalFunds = `$${Number(stateFemaTotalFunds).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
-        var formattedStateCdbgTotalFunds = `$${Number(stateCdbgTotalFunds).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
-        var formattedStatePerCapita = `$${Number(statePerCapita).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
-        var formattedStatePopulation = Number(statePopulation).toLocaleString('en-US', { maximumFractionDigits: 0 });
+    var formattedCountyFemaTotal = `$${Number(countyFemaTotal).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
+    var formattedCountyPerCapita = `$${Number(countyPerCapita).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
+    var formattedStateFemaTotal = `$${Number(stateFemaTotal).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
+    var formattedStateCdbgTotal = `$${Number(stateCdbgTotal).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
+    var formattedStatePerCapita = `$${Number(statePerCapita).toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
+    var formattedStatePopulation = Number(femaFeature.STATE_POPULATION).toLocaleString('en-US', { maximumFractionDigits: 0 });
 
-        var popupContent = `
+    var popupContent = `
     <div class="popup-container">
         <div class="popup-column">
             <h3>${countyName}, ${stateName}</h3>
@@ -164,11 +165,12 @@ map.on('load', function () {
                 <div class="count">${disasterCount}</div>
                 <div class="count-description"># of Federally Declared Extreme Weather Disasters</div>
             </div>
-            <p class="namelsad">FEMA Obligations & HUD CDBG-DR</p>
-            <b>${countyName} Total FEMA:</b> ${formattedFemaCountyTotalFunds}<br>
-            <b>${stateName} Total FEMA:</b> ${formattedStateFemaTotalFunds}<br>
-            <b>${stateName} Total CDBG-DR:</b> ${formattedStateCdbgTotalFunds}<br>
-            <b>${stateName} Population:</b> ${formattedStatePopulation}<br>
+            <p class="namelsad">The Cost of Inaction</p>
+            <b>${countyName} FEMA Total:</b> ${formattedCountyFemaTotal}<br>
+            <b>${countyName} Per Capita:</b> ${formattedCountyPerCapita}<br>
+            <hr>
+            <b>${stateName} FEMA Total:</b> ${formattedStateFemaTotal}<br>
+            <b>${stateName} HUD CDBG Total:</b> ${formattedStateCdbgTotal}<br>
             <b>${stateName} Per Capita:</b> ${formattedStatePerCapita}</p>
             <p class="namelsad">Atlas of Disaster Report</p>
             <p>Read the Atlas of Disaster: ${stateName} to learn more.</p>
@@ -204,12 +206,14 @@ map.on('load', function () {
             </div>
         </div>
     </div>
-        `;
+    `;
 
-        popup.setLngLat(e.lngLat)
-            .setHTML(popupContent)
-            .addTo(map);
-    });
+    popup.setLngLat(e.lngLat)
+        .setHTML(popupContent)
+        .addTo(map);
+});
+
+
 
 
 
